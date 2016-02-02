3fc18dcf7d1290be3802050f61e1dc6fd6c207e0
Piotr Torczyński <ptorczynski@sointeractive.pl>
2016-02-02 08:37:39 +0100

    Inital commit

---
+++ SmogApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmogApp
+//
+//  Created by Piotr Torczyski on 02/02/16.
+//  Copyright © 2016 Piotr Torczyski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
